410cf787aa3fb2ee0dcd1ff11e3468b9e732044c
Mariana Guzmán <mggzmn91@gmail.com>
2020-11-30 18:47:15 -0600

    Filter, sorting and pagination tasks

--- src/models/user.js
+++ src/models/user.js
@@ -2,53 +2,59 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("email invalid");
-      }
+const Task = require("./task");
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(value) {
-      if (value.toLowerCase().includes("password")) {
-        throw new Error("Password cannot contain password");
-      }
-    },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be a positive number");
-      }
-    },
-  },
-  tokens: [
-    {
-      token: {
-        type: String,
-        required: true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("email invalid");
+        }
       },
     },
-  ],
-});
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 7,
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error("Password cannot contain password");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      default: 0,
+      validate(value) {
+        if (value < 0) {
+          throw new Error("Age must be a positive number");
+        }
+      },
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.methods.toJSON = function () {
   const user = this;
@@ -64,6 +70,11 @@ userSchema.methods.generateAuthToken = async function () {
   await user.save();
   return token;
 };
+userSchema.virtual("tasks", {
+  ref: "Task",
+  localField: "_id",
+  foreignField: "owner",
+});
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
   if (!user) {
@@ -81,6 +92,12 @@ userSchema.pre("save", async function (next) {
   if (user.isModified("password")) {
     user.password = await bcrypt.hash(user.password, 8);
   }
+  next();
+});
+
+userSchema.pre("remove", async function (next) {
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
   next();
 });
 const User = mongoose.model("User", userSchema);
--- src/routers/user.js
+++ src/routers/user.js
@@ -7,7 +7,7 @@ router.post("/users", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    const token = user.generateAuthToken();
+    const token = await user.generateAuthToken();
     res.status(201).send({ user, token });
   } catch (e) {
     res.sendStatus(400);
@@ -52,7 +52,7 @@ router.post("/users/logoutAll", auth, async (req, res) => {
   }
 });
 
-router.get("/users/:id", async (req, res) => {
+/* router.get("/users/:id", async (req, res) => {
   try {
     const _id = req.params.id;
     const user = await User.findById(_id);
@@ -63,9 +63,9 @@ router.get("/users/:id", async (req, res) => {
   } catch (e) {
     res.sendStatus(500);
   }
-});
+}); */
 
-router.patch("/users/:id", async (req, res) => {
+router.patch("/users/me", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["name", "email", "password", "age"];
   const isValidOperation = updates.every((update) => {
@@ -75,31 +75,22 @@ router.patch("/users/:id", async (req, res) => {
     return res.status(400).send({ error: "Invalid" });
   }
   try {
-    const user = await User.findById(req.params.id);
     updates.forEach((update) => {
-      user[update] = req.body[update];
+      req.user[update] = req.body[update];
     });
-    await user.save();
-    /* const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    }); */
-    if (!user) {
-      return res.sendStatus(404);
-    }
-    res.send(user);
+    await req.user.save();
+    res.send(req.user);
   } catch (e) {
     res.sendStatus(400);
   }
 });
 
-router.delete("/users/:id", async (req, res) => {
+router.delete("/users/me", auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      res.sendStatus(404);
-    }
-    res.send(user);
+    //const user = await User.findByIdAndDelete(req.user._id);
+    await req.user.remove();
+
+    res.send(req.user);
   } catch (e) {
     res.sendStatus(500);
   }
